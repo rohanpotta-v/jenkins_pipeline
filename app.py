20143fbf4197339a159cd4c8d767d2f6e2adf1e7
rohanpotta-v <rohan.potta@veltris.com>
2025-07-08 15:48:20 +0530

    pushing the dev code

--- app.py
+++ app.py
@@ -4,14 +4,6 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Hello, This is production!"
-
-@app.route('/staging')
-def hello():
-    return "Hello, This is Staging!"
-
-@app.route('/staging')
-def hello():
     return "Hello, This is Devlopment!"
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000)
