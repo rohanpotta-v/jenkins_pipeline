1120e2ac958fd70a715c2d0d7972195fe53e068a
rohanpotta-v <rohan.potta@veltris.com>
2025-07-08 15:46:21 +0530

    pushing to staging

--- app.py
+++ app.py
@@ -2,11 +2,8 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/')
-def hello():
-    return "Hello, This is production!"
 
-@app.route('/staging')
+@app.route('/')
 def hello():
     return "Hello, This is Staging!"
 if __name__ == '__main__':
