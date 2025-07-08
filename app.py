7c0a645003ebe49f7704f3c353585924b173ac7f
rohanpotta-v <rohan.potta@veltris.com>
2025-07-08 15:55:53 +0530

    making changes

--- app.py
+++ app.py
@@ -4,6 +4,6 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Hello, This is Devlopment!"
+    return "Hello, This is Devlopment!, welcome "
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
