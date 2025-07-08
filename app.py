5825710bedbc58358ca6abdb4cb5d453303beb76
rohanpotta-v <rohan.potta@veltris.com>
2025-07-08 17:57:30 +0530

    test cases

--- app.py
+++ app.py
@@ -4,6 +4,6 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Hello, This is Devlopment!, welcome, this is round 2 "
+    return "Hello, This is Devlopment!, i checking if test cases work"
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
